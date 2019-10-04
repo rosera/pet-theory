5f64ab03b3c69d8d0772a485552021ff683bb2a3
rosera <askrichardrose@gmail.com>
2019-10-04 09:21:33 +0100

    test run - 4th Oct

--- lab01/solution/importTestData.js
+++ lab01/solution/importTestData.js
@@ -1,10 +1,30 @@
 const parse       = promisify(require('csv-parse'));
 const {readFile}  = require('fs').promises;
 const {promisify} = require('util');
+const {Firestore} = require('@google-cloud/firestore');
 
 if (process.argv.length < 3) {
   console.error('Please include a path to a csv file');
   process.exit(1);
+}
+
+
+const db = new Firestore();
+
+function writeToFirestore(records) {
+  const batchCommits = [];
+  let batch = db.batch();
+  records.forEach((record, i) => {
+    var docRef = db.collection('customers').doc(record.id);
+    batch.set(docRef, record);
+    if ((i + 1) % 500 === 0) {
+      console.log(`Writing record ${i + 1}`);
+      batchCommits.push(batch.commit());
+      batch = db.batch();
+    }
+  });
+  batchCommits.push(batch.commit());
+  return Promise.all(batchCommits);
 }
 
 function writeToDatabase(records) {
@@ -18,7 +38,8 @@ async function importCsv(csvFileName) {
   const fileContents = await readFile(csvFileName, 'utf8');
   const records = await parse(fileContents, { columns: true });
   try {
-    await writeToDatabase(records);
+    await writeToFirestore(records);
+    //await writeToDatabase(records);
   }
   catch (e) {
     console.error(e);
