ccf62aafd56daa3693acec47e20f763b6eb1e305
rosera <askrichardrose@gmail.com>
2019-10-04 17:52:22 +0100

    Testing WIP - 4th Oct

--- lab03/index.js
+++ lab03/index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const app = express();
+const express    = require('express');
+const app        = express();
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
@@ -16,9 +19,11 @@ app.post('/', async (req, res) => {
   catch (ex) {
     console.log(ex);
   }
-  res.send('OK');
+  res.set('Content-Type', 'text/plain');
+  res.send('\n\nOK\n\n');
 })
 
 function decodeBase64Json(data) {
   return JSON.parse(Buffer.from(data, 'base64').toString());
 }
+
