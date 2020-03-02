e5ee80fa103a638617358329bc245299514d1404
rosie <askrichardrose@gmail.com>
2020-03-02 12:45:16 +0000

    Update deploy.sh

--- lab05/solution/deploy.sh
+++ lab05/solution/deploy.sh
@@ -2,7 +2,7 @@ mkdir common
 cp ../common/* common
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/lab-report-service
-gcloud beta run deploy lab-report-service \
+gcloud run deploy lab-report-service \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/lab-report-service \
   --platform managed \
   --region us-central1 \
