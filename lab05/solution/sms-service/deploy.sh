3aca80fb07c3c76a742a919e08b219e9bb905c3b
rosie <askrichardrose@gmail.com>
2020-03-02 12:36:33 +0000

    Update deploy.sh

--- lab05/solution/sms-service/deploy.sh
+++ lab05/solution/sms-service/deploy.sh
@@ -5,7 +5,7 @@ cp ../common/* common
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/sms-service
 
-gcloud beta run deploy sms-service \
+gcloud run deploy sms-service \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/sms-service \
   --platform managed \
   --region us-central1 \
