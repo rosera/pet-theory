c21845726a0c224894241df9d02374df30d6a7b1
rosera <askrichardrose@gmail.com>
2021-05-12 15:43:39 +0100

    Add example service

--- lab09/apiv1/index.js
+++ lab09/apiv1/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const apiV1 = require('./api-v1.js');
+const path = require('path');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 8080;
 const app = express();
+const pathPublicDirectory = path.join(__dirname, './public'); 
+const pathViews = path.join(__dirname, '/views');
 
 app.use(express.json());
 app.use('/v1', apiV1);
+
+// Set hbs as the template engine
+app.set('view engine', 'hbs');
+app.set('views', pathViews);
+
+// Set the location of the html templates
+app.use(express.static(pathPublicDirectory));     
 
 app.listen(port, () => {
   console.log(`Microservice traffic splitting example: Listening on port ${port}`);
 });
 
-app.get('/', async (req, res) => {
+
+// Render the web page with parameters
+app.get('', (req, res) => {
+  res.render('index');
+})
+
+app.get('/help', async (req, res) => {
   res.status(200).send('API Microservice example');
 })
--- lab09/apiv2/index.js
+++ lab09/apiv2/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const apiV1 = require('./api-v1.js');
 const apiV2 = require('./api-v2.js');
+const path = require('path');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 8080;
 const app = express();
+const pathPublicDirectory = path.join(__dirname, './public'); 
+const pathViews = path.join(__dirname, '/views');
 
 app.use(express.json());
 app.use('/v1', apiV1);
 app.use('/v2', apiV2);
 
+// Set hbs as the template engine
+app.set('view engine', 'hbs');
+app.set('views', pathViews);
+
+app.use(express.static(pathPublicDirectory));     
+
 app.listen(port, () => {
   console.log(`Microservice traffic splitting example: Listening on port ${port}`);
 });
+
+// Render the web page with parameters
+app.get('', (req, res) => {
+  res.render('index');
+})
 
 app.get('/', async (req, res) => {
   res.status(200).send('API Microservice example');
