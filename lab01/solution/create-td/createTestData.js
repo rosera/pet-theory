091c2f853c910d4a198634cadab7488d5d8c5a19
rosera <askrichardrose>
2024-02-28 05:55:11 +0000

    Add: Housekeeping

---
+++ lab01/solution/create-td/createTestData.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const faker = require('faker');
+
+function getRandomCustomerEmail(firstName, lastName) {
+  const provider = faker.internet.domainName();
+  const email = faker.internet.email(firstName, lastName, provider);
+  return email.toLowerCase();
+}
+
+async function createTestData(recordCount) {
+  const fileName = `customers_${recordCount}.csv`;
+  var f = fs.createWriteStream(fileName);
+  f.write('id,name,email,phone\n')
+  for (let i=0; i<recordCount; i++) {
+    const id = faker.datatype.number();
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const name = `${firstName} ${lastName}`;
+    const email = getRandomCustomerEmail(firstName, lastName);
+    const phone = faker.phone.phoneNumber();
+    f.write(`${id},${name},${email},${phone}\n`);
+  }
+  console.log(`Created file ${fileName} containing ${recordCount} records.`);
+}
+
+recordCount = parseInt(process.argv[2]);
+if (process.argv.length != 3 || recordCount < 1 || isNaN(recordCount)) {
+  console.error('Include the number of test data records to create. Example:');
+  console.error('    node createTestData.js 100');
+  process.exit(1);
+}
+
+createTestData(recordCount);
+
---
+++ lab01/solution/import-log-td/importTestData.js
@@ -0,0 +1,75 @@
+const csv = require('csv-parse');
+const fs  = require('fs');
+const { Firestore } = require("@google-cloud/firestore");
+const { Logging }   = require('@google-cloud/logging');
+
+const logName = "pet-theory-logs-importTestData";
+
+// Creates a Logging client
+const logging = new Logging();
+const log     = logging.log(logName);
+const projectId    = 'PROJECT_ID'
+const collectionId = 'customers'
+
+const resource = {
+  type: "global",
+};
+
+if (process.argv.length < 3) {
+  console.error('Please include a path to a csv file');
+  process.exit(1);
+}
+
+async function writeToFirestore(records) {
+  const db = new Firestore({  
+//        projectId: projectId 
+  });
+  const batch = db.batch()
+
+  records.forEach((record)=>{
+    console.log(`Write: ${record}`)
+    const docRef = db.collection(collectionId).doc(record.email);
+    // Create new document
+    // batch.create(docRef, record)
+    // Merge or create new doc
+    batch.set(docRef, record, { merge: true})
+  })
+
+  batch.commit().then(() => {
+    console.log('Batch executed')
+  }).catch(err => {
+    console.log(`Batch error: ${err}`)
+  })
+  return 
+}
+
+async function importCsv(csvFilename) {
+  const parser = csv.parse({ columns: true, delimiter: ',' }, async function (err, records) {
+    if (err) {
+      console.error('Error parsing CSV:', err);
+      return;
+    }
+    try {
+      console.log(`Call write to Firestore`);
+      await writeToFirestore(records);
+      console.log(`Wrote ${records.length} records`);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  }); 
+
+  await fs.createReadStream(csvFilename).pipe(parser);
+
+  // Create a Log entry
+  success_message = `Success: importTestData - Wrote ${records.length} records`;
+  const entry = log.entry(
+    { resource: resource },
+	  { message: `${success_message}` }
+  );
+
+  // Write the Log Entry
+  log.write([entry]);
+}
+
+importCsv(process.argv[2]).catch(e => console.error(e));
